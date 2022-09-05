0603ac588c07a240d3bc201ef48e3597b805d6a9
Trevor Trinh <trevortrinh@berkeley.edu>
2022-09-04 17:28:57 -0700

    inital commit

---
+++ src/pages/landing.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import NavBar from "../components/nav-bar";
+import { Box } from "@chakra-ui/react";
+
+const Landing: NextPage = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default Landing;
